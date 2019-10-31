0026deac7cbe5d767f1471bbd2f65b9429252505
ebel <ebel@equipmozambique.org>
2019-10-31 21:14:57 +0200

    module done

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -86,7 +86,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         Intent infoIntent = new Intent(this, InfoActivity.class);
         startActivity(infoIntent);
-
         return super.onOptionsItemSelected(item);
     }
 
